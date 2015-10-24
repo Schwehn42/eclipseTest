21cb89c36bafa849e4513154cb40c927df7a5389
Jakob Schwehn <schwehn42@gmail.com>
2015-10-24 15:33:25 +0200

    constructor with support for parents

--- learning_classes_with_class/src/human/Human.java
+++ learning_classes_with_class/src/human/Human.java
@@ -1,13 +1,24 @@
 package human;
 
+import relatives.Parents;
+
 public class Human {
 
 	private Name name;
 	private Gender gender;
+	private Parents parents;
+	
+	public Human() {};
 	
 	public Human(Gender gender, Name name) {
 		this.gender = gender;
 		this.name = name;
+	}
+	
+	public Human(Gender gender, Name name, Parents parents) {
+		this.gender = gender;
+		this.name = name;
+		this.parents = parents;
 	}
 	
 	public static void main(String[] args) {
