b9a66b9b0bcc7929b25d9763ef06a9ba4c1b3456
Jakob Schwehn <schwehn42@gmail.com>
2015-10-25 17:42:52 +0100

    added age

--- learning_classes_with_class/src/human/Human.java
+++ learning_classes_with_class/src/human/Human.java
@@ -1,7 +1,7 @@
 package human;
 
-import genes.Properties;
-import genes.Property;
+//import genes.Properties;
+//import genes.Property;
 import genes.PropertyList;
 import relatives.Parents;
 
@@ -9,37 +9,49 @@ public class Human {
 
 	private Name name;
 	private Gender gender;
+	private int age;
 	private Parents parents;
 	private boolean hasParents = false; //if false, new Properties are generated, else randomly taken from the parents
 	private PropertyList propertyList; //Properties are stored in here
 	
 	public Human() {};
 	
-	public Human(Gender gender, Name name) {
+	public Human(Gender gender, Name name, int age) {
+		System.out.println(name.fullName + " (" + gender + "), " + age);
 		this.gender = gender;
 		this.name = name;
+		this.age = age;
 		
 		this.propertyList = new PropertyList().generatePropertyList();
 	}
 	
-	public Human(Gender gender, Name name, Parents parents) {
+	public Human(Gender gender, Name name, int age, Parents parents) {
+		System.out.println(name.fullName + " (" + gender + "), " + age + " | Parents: " + parents.getParent1().getName().fullName + ", " + parents.getParent2().getName().fullName);
 		this.gender = gender;
 		this.name = name;
 		this.parents = parents;
 		this.hasParents = true;
+		this.age = age;
 		
 		this.propertyList = new PropertyList().mergePropertyLists(this.parents);
 	}
 	
 	public static void main(String[] args) {
-		Human h1 = new Human(Gender.MALE, new Name("Jakob", "Bozz", "Schwan"));
-		Human h2 = new Human(Gender.FEMALE, new Name("Taylor", "Swift"));
-		Human h3 = new Human(Gender.MALE, new Name("Barrack", "Obama"), new Parents(h1, h2));
+		Human h1 = new Human(Gender.MALE, new Name("Jakob", "Bozz", "Schwan"), 17);
+		Human h2 = new Human(Gender.FEMALE, new Name("Taylor", "Swift"), 18);
+		Human h3 = new Human(Gender.MALE, new Name("Barrack", "Obama"), 42, new Parents(h1, h2));
 		
 		h1.getPropertyList().print();
 		h2.getPropertyList().print();
 		h3.getPropertyList().print();
 	}
+	
+	
+	
+	
+	
+	
+	
 	
 	public Name getName() {
 		return this.name;
@@ -55,5 +67,8 @@ public class Human {
 	}
 	public PropertyList getPropertyList() {
 		return this.propertyList;
+	}
+	public int getAge() {
+		return this.age;
 	}
 }
